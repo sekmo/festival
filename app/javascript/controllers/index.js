87aedfb60cf00f0424422b25d4bd64701975fa7b
Francesco Mari <francescojjmari@gmail.com>
2023-10-02 21:19:13 +0200

    Add FavoriteToggleController and associate it to the section element

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
+import FavoriteToggleController from "./favorite_toggle_controller.js"
+application.register("favorite-toggle", FavoriteToggleController)
+
+import HelloController from "./hello_controller.ts"
 application.register("hello", HelloController)
