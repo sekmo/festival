70e21077901410008eed4828628232e7b2db1986
Francesco Mari <francescojjmari@gmail.com>
2023-10-02 19:24:26 +0200

    Remove unuseful comments

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,3 @@
-#---
-# Excerpted from "Modern Front-End Development for Rails, Second Edition",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit https://pragprog.com/titles/nrclient2 for more book information.
-#---
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
