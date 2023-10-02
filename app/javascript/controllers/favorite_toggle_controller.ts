f118f1f854a1f2cff16e51892057869b42694844
Francesco Mari <francescojjmari@gmail.com>
2023-10-02 23:31:16 +0200

    Add the toggle action to the Stimulus controller and hook it to a new "Hide" button

--- app/javascript/controllers/favorite_toggle_controller.ts
+++ app/javascript/controllers/favorite_toggle_controller.ts
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="favorite-toggle"
 export default class FavoriteToggleController extends Controller {
-  connect(): void {
-    console.log("The controller is connected");
+  toggle(): void {
+    console.log("The button was clicked");
   }
 }
