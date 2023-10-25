bd2003cd182584676d84700f2e588a28a1e8e26a
Francesco Mari <francescojjmari@gmail.com>
2023-10-25 09:50:59 +0200

    Add the visibleValue to keep the state, change the button text and toggle the favorite concerts visible class accordingly

--- app/javascript/controllers/favorite_toggle_controller.ts
+++ app/javascript/controllers/favorite_toggle_controller.ts
@@ -2,10 +2,38 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="favorite-toggle"
 export default class FavoriteToggleController extends Controller {
-  static targets = ["elementToHide"]
+  static targets = ["elementToHide", "elementWithText"]
   elementToHideTarget: HTMLElement
+  elementWithTextTarget: HTMLElement
+
+  static values = { visible: Boolean }
+  visibleValue: boolean
+
+  connect(): void {
+    this.updateHiddenClass()
+    this.updateText()
+  }
 
   toggle(): void {
-    this.elementToHideTarget.classList.toggle("hidden")
+    this.flipState()
+    this.updateHiddenClass()
+    this.updateText()
+  }
+
+  flipState(): void {
+    this.visibleValue = !this.visibleValue
+  }
+
+  updateHiddenClass(): void {
+    // Assign the class if visible is false
+    this.elementToHideTarget.classList.toggle("hidden", !this.visibleValue)
+  }
+
+  newText(): string {
+    return this.visibleValue ? 'Hide' : 'Show';
+  }
+
+  updateText(): void {
+    this.elementWithTextTarget.innerText = this.newText()
   }
 }
