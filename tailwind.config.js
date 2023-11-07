b2f6b257087d1cae70d9bf2ddd2557296e6bde64
Francesco Mari <francescojjmari@gmail.com>
2023-11-07 09:00:28 +0100

    Add React, configure eslint and tailwind for React

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     "./app/views/**/*.turbo_stream.erb",
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
+    "./app/javascript/**/*.tsx",
+    "./app/javascript/**/*.ts",
     "./config/initializers/simple_form_tailwind.rb",
   ],
 }
